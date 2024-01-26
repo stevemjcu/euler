492ed4c8181ddc8f96d828732a3d5a0db5a14e28
Stephen Justice <stephenmjcu@gmail.com>
2024-01-26 18:01:35 -0500

    Refactor solution 19 a bit

--- cmd/19/main.go
+++ cmd/19/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
-var daysInMonth = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-var daysOfWeek = []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+var daysPerMonth = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
 type Date struct {
 	weekday, day int
 	month, year  int
 }
+
+func (d Date) Weekday() int { return d.weekday + 1 }
+func (d Date) Day() int     { return d.day + 1 }
+func (d Date) Month() int   { return d.month + 1 }
+func (d Date) Year() int    { return d.year }
 
 func (d Date) Next() Date {
 	d.weekday = (d.weekday + 1) % 7
@@ -19,7 +24,7 @@ func (d Date) Next() Date {
 	if d.month == 1 && d.IsLeapYear() {
 		leap = 1
 	}
-	d.day = (d.day + 1) % (daysInMonth[d.month] + leap)
+	d.day = (d.day + 1) % (daysPerMonth[d.month] + leap)
 	if d.day == 0 {
 		d.month = (d.month + 1) % 12
 		if d.month == 0 {
@@ -34,21 +39,36 @@ func (d Date) IsLeapYear() bool {
 }
 
 func (d Date) String() string {
-	s := daysOfWeek[d.weekday] + " "
-	s += strconv.Itoa(d.day+1) + "-"
-	s += strconv.Itoa(d.month+1) + "-"
-	s += strconv.Itoa(d.year)
+	s := strconv.Itoa(d.Day()) + "-"
+	s += strconv.Itoa(d.Month()) + "-"
+	s += strconv.Itoa(d.Year())
 	return s
 }
 
-func main() {
+func solveCustom() int {
 	count, d := 0, Date{1, 0, 0, 1900}
 	for i := 0; d.year < 2001; i++ {
-		if d.year > 1900 && d.day == 0 && d.weekday == 0 {
+		if d.Year() > 1900 && d.Day() == 1 && d.Weekday() == 1 {
 			fmt.Println(d)
 			count++
 		}
 		d = d.Next()
 	}
-	fmt.Println(count)
+	return count
+}
+
+func solveBuiltIn() int {
+	count, d := 0, time.Date(1901, 1, 1, 0, 0, 0, 0, time.UTC)
+	for d.Year() < 2001 {
+		if d.Day() == 1 && d.Weekday() == time.Sunday {
+			fmt.Println(d)
+			count++
+		}
+		d = d.Add(time.Hour * 24)
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(solveCustom())
 }
