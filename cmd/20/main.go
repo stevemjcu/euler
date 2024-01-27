c8dc553c5723167e1e6e1f3e0394d86ed47aa464
Stephen Justice <stephenmjcu@gmail.com>
2024-01-27 17:28:43 -0500

    Solve problem 20

---
+++ cmd/20/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+func toDigits(n int) []int {
+	var res []int
+	for ; n != 0; n /= 10 {
+		res = append([]int{n % 10}, res...)
+	}
+	return res
+}
+
+func add(a, b []int) []int {
+	if len(a) < len(b) {
+		a, b = b, a
+	}
+	z := make([]int, len(a)-len(b))
+	b = append(z, b...) // pad with 0
+	var c []int
+	carry := 0
+	for i := len(a) - 1; i >= 0; i-- {
+		x := a[i] + b[i] + carry
+		carry, x = x/10, x%10
+		c = append([]int{x}, c...)
+	}
+	if carry != 0 {
+		c = append([]int{carry}, c...)
+	}
+	return c
+}
+
+func multiply(a, b []int) []int {
+	var d []int
+	for i := len(b) - 1; i >= 0; i-- {
+		c := make([]int, len(b)-1-i)
+		carry := 0
+		for j := len(a) - 1; j >= 0; j-- {
+			x := a[j]*b[i] + carry
+			carry, x = x/10, x%10
+			c = append([]int{x}, c...)
+		}
+		if carry != 0 {
+			c = append([]int{carry}, c...)
+		}
+		d = add(c, d)
+	}
+	return d
+}
+
+func factorial(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func factorialBigInt(n int) []int {
+	if n == 1 {
+		return []int{1}
+	}
+	a, b := toDigits(n), factorialBigInt(n-1)
+	return multiply(a, b)
+}
+
+func main() {
+	sum := 0
+	for _, x := range factorialBigInt(100) {
+		sum += x
+	}
+	fmt.Println(sum)
+}
